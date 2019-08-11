1fb168f1b78a069cdc6f9e01f293c6d674a83c4b
Alexandra <alexandrita@ukr.net>
2019-08-11 12:56:43 +0300

    Add

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
-let topping = [];
+class Ingredients {
+  constructor(price, calories) {
+    this.price = price;
+    this.calories = calories;
+  }  
+}
+
+let SIZE_SMALL = new Ingredients(50, 20);
+let SIZE_BIG = new Ingredients(100, 40);
+let STUFFING_CHEESE = new Ingredients(10, 20);
+let STUFFING_SALATE = new Ingredients(20, 5);
+let TOPPING_POTETOES = new Ingredients(15, 10);
+let TOPPING_SAUCE = new Ingredients(15, 0);
+let TOPPING_MAYO = new Ingredients(20, 5);
+
 
 class Hamburger {
   constructor(size, stuffing) {
     this.size = size;
     this.stuffing = stuffing;
+    this.topping = [];
   }
 
-  static SIZE_SMALL = [50, 20];
-  static SIZE_BIG = [100, 40];
-  static STUFFING_CHEESE = [10, 20];
-  static STUFFING_SALATE = [20, 5];
-  static TOPPING_POTETOES = [15, 10];
-  static TOPPING_SAUCE = [15, 0];
-  static TOPPING_MAYO = [20, 5];
-
-  addTopping(toppingItem){      
-    return topping.push(toppingItem);
-  }
+  addTopping(toppingItem){ 
+    return this.topping.push(toppingItem);
+  };
 
   calculateCalories(){
-  return topping.reduce(function(sum, current) {    
-      return sum + +current[1];   
-    }, this.size[1] + this.stuffing[1]);
-  }
+  return this.topping.reduce(function(sum, current) {    
+      return sum + current.calories;   
+    }, this.size.calories + this.stuffing.calories);
+  };
 
   calculatePrice(){
-    return topping.reduce(function(sum, current) {    
-        return sum + +current[0];   
-    }, this.size[0] + this.stuffing[0]);
-  }
+    return this.topping.reduce(function(sum, current) {    
+        return sum + current.price;   
+    }, this.size.price + this.stuffing.price);
+  };
 
 }
 
-let hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+
+let hamburger = new Hamburger(SIZE_SMALL, STUFFING_CHEESE);
 console.log(hamburger);
-hamburger.addTopping(Hamburger.TOPPING_MAYO);
+hamburger.addTopping(TOPPING_MAYO);
 console.log("Calories: " + hamburger.calculateCalories());
 console.log("Price: " + hamburger.calculatePrice());
-hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+hamburger.addTopping(TOPPING_SAUCE);
 console.log("Calories: " + hamburger.calculateCalories());
 console.log("Price: " + hamburger.calculatePrice());
